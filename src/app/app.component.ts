d3fb8351eb71297c0617c4e195936f23ba133c36
Mike McGuire <mcguire@mullenhigh.com>
2016-06-22 11:13:25 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import {AttributeDirectivesComponent} from "./attribute-directives.component";
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  template: `
+      <my-attribute-directives></my-attribute-directives>
+    `,
+  directives: [AttributeDirectivesComponent]
 })
 export class AppComponent {
   title = 'app works!';
---
+++ src/app/attribute-directives.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'my-attribute-directives',
+  template: `
+    <dir>
+        Highlight me
+    </dir>
+    `
+
+})
+
+export class AttributeDirectivesComponent {
+
+}
