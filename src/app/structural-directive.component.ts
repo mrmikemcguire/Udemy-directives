760a809feea627cf0d550401934ee8c943cdb958
Mike McGuire <MoonshotMike@gmail.com>
2016-07-06 11:12:57 -0600

    all but css working at end of lecture 35

--- src/app/structural-directive.component.ts
+++ src/app/structural-directive.component.ts
@@ -3,7 +3,18 @@ import { Component } from '@angular/core';
 @Component ({
   selector: 'my-structural-directives',
   template: `
-
+    <section class="directive">
+      <h2>*ngIf</h2>
+      <div>
+        Enter a number higher than ten 
+        <br>
+        <input type="text" #number (keyup)="0">
+      </div>
+      <div *ngIf="number.value > 10">
+          <h5>Any children follow their parent</h5>
+        Number is greater than ten   
+      </div>
+    </section>
 `
 
 })
